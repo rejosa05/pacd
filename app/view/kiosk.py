b945175f1ec483e51ba03fb7008a88d182c68fe7
Rolly <rejosa05@gmail.com>
2026-03-30 16:59:50 +0800

    update

---
+++ app/view/kiosk.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from forms import ClientDetailsForm
+from utils import send_queue_update
+
+
+def client_details(request):
+    if request.method == 'POST':
+        form = ClientDetailsForm(request.POST)
+        if form.is_valid():
+            client = form.save()
+
+            # 🔥 REAL-TIME UPDATE
+            send_queue_update(client)
+
+            return redirect('client_ticket', client_id=client.id)
+    else:
+        form = ClientDetailsForm()
+
+    return render(request, 'app/client.html', {'form': form})
