676a9da2eb7e4a7db39d76af3df90a6c4f6f2a47
Rolly <rejosa05@gmail.com>
2026-07-24 14:32:56 +0800

    add kiosk & transaction

--- app/views.py
+++ app/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def kiosk(request):
+    return render(request, 'pages/kiosk.html')
+
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
+
+def transaction(request):
+    return render(request, 'pages/transaction.html')
--- pacd/urls.py
+++ pacd/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, re_path as path
-from app.views import index, dashboard
+from app.views import index, dashboard, kiosk, transaction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('index', index, name="index"),
-    path('dashboard_', dashboard, name="dashboard_")
+    path('kiosk_', kiosk, name="kiosk_"),
+    path('dashboard_', dashboard, name="dashboard_"),
+    path('transaction_', transaction, name="transaction_")
 ]
